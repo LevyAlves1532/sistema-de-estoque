1bb14504b3ddf230e2dec3d63947f11e8eb16046
Lêvy <levy.pereiraA1532@gmail.com>
2024-03-27 05:54:24 -0300

    Remove csrf from forms, remove console.log from home controller

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -13,8 +13,6 @@ exports.index = async (req, res) => {
     products = await new ProductsModel().getAllFromIdUser(req.session.user._id);
   }
 
-  console.log(products);
-
   res.render("index", { 
     categories, 
     products: products.map(product => {
